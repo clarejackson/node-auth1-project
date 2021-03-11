54e7b8cd4d2cc6957a6b7e036fb8dc8f3196cdc3
Clare Jackson <clare-jackson@lambdastudents.com>
2021-03-10 21:40:19 -0500

    users model, deps and server.js sone

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+// const usersRouter = require("./users/users-router");
+// const authRouter = require("./auth/auth-router");
+const db = require("../data/db-config");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -20,6 +25,18 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session({
+  resave: false, // avoid recreating sessions that have not changed
+  saveUninitialized: false, // for laws against setting cookies automatically
+  secret: "keep it secret keep it safe", // cryptographically sign the session/cookie
+  store: new KnexSessionStore({
+    knex: db, // pass configured instance of knex
+    createtable: true, // if the session table does not exist, create it
+  })
+}))
+
+// server.use(usersRouter)
+// server.use(authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
